ed8488a5b58f5c501fffb8a4acd9926c12c5a7d1
Jongseok <js21c@Jongseokui-MacBook-Pro.local>
2017-06-13 23:39:38 +0900

    Modify main file. Support train set director argument

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import numpy as np
 import src.read_data as read_data
+import sys
 
 def prepare_data(path_train):
-     print "read from ", path_train
      train_set = read_data.load_jpeg_data_in_fixed_size(path_train, 320, 240)
      num_train, w, h = train_set.shape
 
@@ -14,5 +14,12 @@ def prepare_data(path_train):
          return train_set
 
 if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print "Usage : python main.py [TrainSet_Directory]"
+        sys.exit()
+    else:
+        path_train = sys.argv[1]
+        print "TrainSet Directory :", path_train
+        
     prepare_data('/Users/js21c/jscode/SPaC_ML/dataset/benz/')
 
