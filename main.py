8b75f51672430b78bcb7772c34f16b89ad967460
merongyong <bagbeom-yong@bagbeom-yong-ui-MacBook-Pro.local>
2017-06-13 23:22:54 +0900

    Revise print func for python3

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-import numpy as np
 import src.read_data as read_data
 
 
 def prepare_data(path_train):
-	print "read from ", path_train
+	print("read from ", path_train)
 	train_set = read_data.load_jpeg_data_in_fixed_size(path_train, 320, 240)
 
 
 if __name__ == "__main__":
-    prepare_data('/Users/js21c/jscode/SPaC_ML/dataset/benz/')
-
+    prepare_data('/Users/bagbeom-yong/Workspace/spac_ml/SPaC_ML/dataset/benz/')
